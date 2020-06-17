f20c302b5a2e174b1418b735bb9c2758fcc5a3ed
shiz0 <info@h2-it.de>
2020-06-17 08:41:25 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
-if [ "$BACKUP_INTERVAL" = "5min" ]; then
-	mkdir /etc/periodic/"$BACKUP_INTERVAL"
-fi
-if [ "$BACKUP_INTERVAL" = "custom" ]; then
+if [ "$BACKUP_INTERVAL" = "5min" ] || [ "$BACKUP_INTERVAL" = "custom" ]; then
 	mkdir /etc/periodic/"$BACKUP_INTERVAL"
 fi
 
 ln -s /root/backup.sh /etc/periodic/"$BACKUP_INTERVAL"/backup
 
 export > /container.env
-
-chown -R root:root /root
-
 exec "$@"
